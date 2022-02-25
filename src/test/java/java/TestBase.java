2d2ca32f76ee96760447b55cee22083c17a0147b
h e <elhab11>
2022-02-25 17:08:43 -0500

    refacture testHeight

--- src/test/java/java/TestBase.java
+++ src/test/java/java/TestBase.java
@@ -1,6 +1,5 @@
 package java;
 
-
 import io.opentelemetry.exporter.logging.SystemOutLogExporter;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -16,33 +15,23 @@ import org.asynchttpclient.Response;
 import org.junit.Test;
 
 public class TestBase {
-    
-   public RequestSpecification httprequest;
-  
-   
-   
 
-    public TestBase() {
-        try {
-            Properties props = new Properties();
-            props.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
+	public RequestSpecification httprequest;
 
-            //Rest Assured config
-            RestAssured.baseURI = props.getProperty("api.uri");
-            httprequest = RestAssured.given();
-            io.restassured.response.Response resp =  httprequest.get("people");
-           
-           
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-       
-        }
-  
-         
-         
-    
+	public TestBase() {
+		try {
+			Properties props = new Properties();
+			props.load(getClass().getClassLoader().getResourceAsStream("config.properties"));
 
+			// Rest Assured config
+			RestAssured.baseURI = props.getProperty("api.uri");
+			httprequest = RestAssured.given();
+			io.restassured.response.Response resp = httprequest.get("people");
 
-    }
-    
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
--- src/test/java/java/testRestAssured.java
+++ src/test/java/java/testRestAssured.java
@@ -14,62 +14,60 @@ import io.restassured.response.ResponseBodyExtractionOptions;
 import io.restassured.specification.RequestSpecification;
 
 public class testRestAssured extends TestBase {
-	
-
-	
 
 	@Test
 	public void testResponStatus() {
-		 io.restassured.response.Response resp =  httprequest.get("people");
-		 resp.then().statusCode(200);
+		io.restassured.response.Response resp = httprequest.get("people");
+		resp.then().statusCode(200);
 	}
+
 	@Test
 	public void testHeight() {
-		  io.restassured.response.Response resp =  httprequest.get("people");
-          JsonPath jsonResponse = resp.jsonPath();
+		io.restassured.response.Response resp = httprequest.get("people");
+		JsonPath jsonResponse = resp.jsonPath();
 
-        //  System.out.println(jsonResponse.get("results.height")+"hereeeeee");
-          ArrayList<Integer> heightList = jsonResponse.get("results.height");
-         // int arr[]=jsonResponse.get("results.height");
-          System.out.println(heightList);
-             int count=0;
-          for (Integer var : heightList) 
-          { 
-        	
-              if (var>=200) {
-            	  count++;
-              }
-          }
-          assertTrue(count==10);
-        
+		ArrayList<String> heightList = jsonResponse.get("results.height");
+
+		System.out.println(heightList.size());
+		int count = 0;
+		for (String var : heightList) {
+			int intValue = Integer.parseInt(var);
+			if (intValue > 200) {
+				count++;
+
+			}
+		}
+		assertTrue(count == 10);
+
 	}
+
 	@Test
 	public void returnedNames() {
-		  io.restassured.response.Response resp =  httprequest.get("people");
-          JsonPath jsonResponse = resp.jsonPath();
+		io.restassured.response.Response resp = httprequest.get("people");
+		JsonPath jsonResponse = resp.jsonPath();
 
-       
-          ArrayList<String> names = jsonResponse.get("results.name");
-          String[] GivenNames = {"Darth Vader", "Chewbacca", "Roos Tarpals", "Rugor Nass", "Yarael Poof", "Lama Su", "Tuan Wu", "Grievous", "Tarfful", "Tion Medon"};
-          System.out.println(names);
-             int count=0;
-             boolean condition= true;
-          for (String var : GivenNames) 
-          { 
-        	  System.out.println(var);
-        	  if (names.contains(var)==false) {
-                 condition = false;
-                 break;
-              }
-              }
-          assertTrue(condition);
-          }
+		ArrayList<String> names = jsonResponse.get("results.name");
+		String[] GivenNames = { "Darth Vader", "Chewbacca", "Roos Tarpals", "Rugor Nass", "Yarael Poof", "Lama Su",
+				"Tuan Wu", "Grievous", "Tarfful", "Tion Medon" };
+		System.out.println(names);
+
+		boolean condition = true;
+		for (String var : GivenNames) {
+			System.out.println(var);
+			if (names.contains(var) == false) {
+				condition = false;
+				break;
+			}
+		}
+		assertTrue(condition);
+	}
+
 	@Test
 	public void peopleChecked() {
-		io.restassured.response.Response resp =  httprequest.get("people");
-        JsonPath jsonResponse = resp.jsonPath();
-        Integer checked = jsonResponse.get("count");
-        assertTrue(checked==82);
+		io.restassured.response.Response resp = httprequest.get("people");
+		JsonPath jsonResponse = resp.jsonPath();
+		Integer checked = jsonResponse.get("count");
+		assertTrue(checked == 82);
 	}
-	
+
 }
